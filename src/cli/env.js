2eb3d1eba2330fb78c72b978ead78da9ca4373b9
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-06-17 00:28:35 +0300

    feat: add cli solution

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
-const parseEnv = () => {
-    // Write your code here 
+const parseEnv = (prefix) => {
+    process.stdout.write(Object.entries(process.env)
+        .filter(([prop, _]) => prop.startsWith(prefix))
+        .map(([prop, value]) => `${prop}=${value}`)
+        .join('; ')
+    );
 };
 
-parseEnv();
+parseEnv('RSS_');
