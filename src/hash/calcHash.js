2e0664639c4a0ae9334a2a2a1a22eaee05c1f980
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-06-17 10:21:38 +0300

    feat: add hash solution

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,21 @@
-const calculateHash = async () => {
-    // Write your code here 
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import crypto from 'crypto';
+
+const fileName = 'fileToCalculateHashFor.txt';
+const dirName = 'files';
+const hashName = 'sha256';
+const encodingName = 'hex';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, dirName, fileName);
+
+const calculateHash = async (filePath, hashName, encodingName) => {
+    fs.createReadStream(filePath)
+        .pipe(crypto.createHash(hashName))
+        .setEncoding(encodingName)
+        .pipe(process.stdout)
 };
 
-await calculateHash();
+await calculateHash(filePath, hashName, encodingName);
