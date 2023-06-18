c90390c4eb8515a729beddcb6df7a159d19cc514
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-06-18 15:42:46 +0300

    feat: add cp colution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import cp from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+import os from 'os';
+
+const cpFileName = 'script.js';
+const dirName = 'files';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const cpFilePath = path.join(__dirname, dirName, cpFileName);
+
+const spawnChildProcess = async (args, cpFile) => {
+    
+    const worker = cp.fork(cpFile, args);
+    worker.on('error', () => process.exit(0));
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess( [1, 2], cpFilePath);
