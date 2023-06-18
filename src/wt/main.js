ef7e9c5bef6b94b0b256555d18dedf332d3f97e7
Mila Bozhevskaya <mi.bozhev@gmail.com>
2023-06-18 14:35:51 +0300

    feat: add wt solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
-const performCalculations = async () => {
-    // Write your code here
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+import os from 'os';
+
+const workerFileName = 'worker.js';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const workerFilePath = path.join(__dirname, workerFileName);
+const number = 10;
+
+const performCalculations = async (number, workerFile) => {
+    
+    const cpuCount = os.cpus().length;
+    const workers = [];
+    for (let i = 0; i <= cpuCount; i++) {
+        workers.push(new Promise((res, rej) => {
+            const worker = new Worker(workerFile, { workerData: number + i });
+            worker.on('message', (result) => res(result));
+            worker.on('error', (error) => rej(error));
+            worker.on('exit', () => {});
+        }))
+    }
+    const workersResult = (await Promise.allSettled(workers)).map(({ status, value }) => ({
+        status: status === 'fulfilled' ? 'resolved' : 'error',
+        data: status === 'fulfilled' ? value : null,
+    }));
+    console.log(workersResult);
 };
 
-await performCalculations();
+await performCalculations(number, workerFilePath);
